b4557cd5105aec86850fc4d4baa52d87c3d9423b
Claudia Amarilla <clauamarilla2009@gmail.com>
2025-02-04 17:36:01 -0300

    trabajando en sus carpetas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const audioIcon = document.querySelector('.audio-icon');
+    const audioPlayer = document.getElementById('audioPlayer');
 
-// Selecciona el ícono y el reproductor de audio
-const audioIcon = document.querySelector('.audio-icon');
-const audioPlayer = document.getElementById('audioPlayer');
-
-// Añade un evento de clic al ícono
-audioIcon.addEventListener('click', function() {
-    if (audioPlayer.paused) {
-        audioPlayer.play(); // Reproduce el audio si está pausado
-    } else {
-        audioPlayer.pause(); // Pausa el audio si está reproduciéndose
+    if (audioIcon && audioPlayer) {
+        audioIcon.addEventListener('click', function() {
+            if (audioPlayer.paused) {
+                audioPlayer.play(); // Reproduce el audio si está pausado
+            } else {
+                audioPlayer.pause(); // Pausa el audio si está reproduciéndose
+            }
+        });
     }
 });
+
+
